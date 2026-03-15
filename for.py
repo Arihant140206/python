c2555779eb6166c4e243eff508df9b0dd03a7553
Arihant Jain <arihantjain140602@gmail.com>
2026-03-15 14:55:14 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,9 @@
+exp=[300,500,600,200]
+total=0
+for item in exp:# for item in range(len(exp)):  can also be written here
+    total=total+item
+print(total)
+
+#for i in range(1,11)
+#print i -----> prints 1 to 10
+
